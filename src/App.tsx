86e574a19e5863ef678a6c2b2656a3361daaa5ac
albert.jon.inciong.0915 <albert.jon.inciong.0915@gmail.com>
2025-02-11 22:47:51 +0800

    added full data accordions on bento

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,55 @@
 // import { useState } from "react";
 import "./App.css";
-import AccordionComponent, { itemI } from "./components/Accordion";
+import AccordionComponent, { itemI, FullDataI } from "./components/Accordion";
 import useFetchAllData from "./hooks/useFetchAllData";
 import useFetchDataById from "./hooks/useFetchDataById";
 import useFetchCurrentData from "./hooks/useFetchCurrentData";
 import ApiService from "./utils/useApi";
 import { useCallback, useEffect, useState } from "react";
 
+
 function App() {
      const { useApi } = ApiService();
      const { fullData } = useFetchAllData();
-     const { datasById } = useFetchDataById("3"); // change depending on which id
+     const { datasById } = useFetchDataById("1"); // change depending on which id
      const { currentData } = useFetchCurrentData("3"); // change depending on which id
 
-     const [selections, setSelections] = useState([])
+     const [selections, setSelections] = useState<string[]>([])
+     const [flattenDataArr, setFlattenDataArr] = useState<FullDataI[]>([])
 
-     const transformedData = fullData?.map((item) => ({
+     const transformedData = datasById?.map((item) => ({
           voltage: Number(item.voltage),
           current: Number(item.current),
           timeStamp: item.timeStamp,
      }));
+     
+     const flattenData = (dataCollection: any[]): FullDataI[] => {
+          if (dataCollection === null || dataCollection.length === 0) return [];
+      
+          return dataCollection.flatMap((item: any) => {
+            const { timeStamp } = item;
+            return item.sensorData.map((sensorItem: any) => ({
+              timeStamp: timeStamp,
+              sensorClientId: sensorItem.sensorClientId,
+              voltage: sensorItem.monitoredData.voltage,
+              current: sensorItem.monitoredData.current
+            }));
+          });
+        };
+      
+        useEffect(() => {
+          if (flattenDataArr.length === 0) {
+            const flattenedData = flattenData(fullData as any[]);
+            setFlattenDataArr(flattenedData); // Proper state update after flattening
+          }
+        }, [flattenDataArr.length, fullData]); 
 
      const selectionsById = useCallback(async () => {
           const api = useApi(`EE/client-selections`);
           const resp = await api.get()
 
           if(resp){
+               console.log(resp.espClientId)
                setSelections(resp.espClientId)
                return
           }
@@ -35,15 +59,28 @@ function App() {
           selectionsById()
      }, [])
 
-     // return <AccordionComponent ArrOfData={transformedData as itemI[]} />;
      return (
           <>
                <div className="parent">
-                    <div className="div1"> graph</div>
-                    <div className="div2"> list all</div>
-                    <div className="div3"> current </div>
-                    <div className="div4">list by id </div>
-                    <div className="div5">bar graph </div>
+                    <div className="div1 border rounded" > graph</div>
+                    <div className="div2 p-2 border rounded" style={{ overflowX: "hidden", overflowY: "auto"}} >
+                         <div className="px-1 p-1 text-black fs-3">
+                              All data
+                         </div>
+                         {
+                              flattenDataArr !== null && flattenDataArr.length > 0 ?
+                              <AccordionComponent ArrOfData={flattenDataArr} styling={{background: '#eff5ff'}}/>
+                               : 
+                              <div className="text-center mt-5">Loading ....</div>
+                         }
+                    </div>
+                    {/* <div className="div3">
+                         <div className="px-1 p-1 text-black fs-3">
+                              Current
+                         </div>
+                    </div> */}
+                    <div className="div3 border rounded">bar graph</div>
+                    <div className="div4 border rounded">current by id</div>
                </div>
           </>
      )
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 
 export interface itemI {
@@ -6,26 +7,36 @@ export interface itemI {
      timeStamp: string;
 }
 
-function AccordionComponent({ArrOfData}: { ArrOfData: itemI[] }) {
+export interface FullDataI extends itemI {
+     sensorClientId?: string
+}
+
+function AccordionComponent({ArrOfData, styling}: { ArrOfData: FullDataI[] | itemI[] , styling?: React.CSSProperties }) {
      return (
           <Accordion>
                {ArrOfData &&
-                    ArrOfData.map((item: itemI, index: number) => {
+                    ArrOfData.map((item: FullDataI, index: number) => {
                          return (
-                              <Accordion.Item eventKey={`${index}`} key={index}>
-                                   <Accordion.Header>{item.timeStamp}</Accordion.Header>
+                              <Accordion.Item style={{...styling}}  eventKey={`${index}`} key={index}>
+                                   <Accordion.Header>
+                                        <div className="d-flex flex-column">
+                                             <div>{item.timeStamp}</div>
+                                             <div className="text-secondary fw-light" style={{ fontSize: "14px" }}>
+                                                  esp32 Client {item.sensorClientId}
+                                             </div>
+                                        </div>
+                                   </Accordion.Header>
                                    <Accordion.Body>
                                         <div className="text-secondary">
-                                             Voltage: <span className="text-primary">{item.voltage}</span>
+                                             Voltage: <span className="text-black fw-medium">{item.voltage}</span>
                                         </div>
                                         <div className="text-secondary">
-                                             Current: <span className="text-success">{item.current}</span>
+                                             Current: <span className="text-black fw-medium">{item.current}</span>
                                         </div>
                                    </Accordion.Body>
                               </Accordion.Item>
                          );
                     })}
-               )
           </Accordion>
      );
 }
