9540223226151710e1fd3d6c5a065212cb5bb8a7
albert.jon.inciong.0915 <albert.jon.inciong.0915@gmail.com>
2025-02-12 14:39:41 +0800

    add current by id bento box

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-// import { useState } from "react";
 import "./App.css";
 import AccordionComponent, { FullDataI } from "./components/Accordion";
-import MyCard from "./components/Card";
+import MyCard, { CardsI } from "./components/Card";
 import useFetchAllData from "./hooks/useFetchAllData";
 import { flattenData } from "./utils";
-// import useFetchDataById from "./hooks/useFetchDataById";
-// import useFetchCurrentData from "./hooks/useFetchCurrentData";
+import useFetchCurrentData from "./hooks/useFetchCurrentData";
 import { useEffect, useState } from "react";
 
 
 function App() {
      const { fullData } = useFetchAllData();
-     // const { datasById } = useFetchDataById("1"); // change depending on which id
-     // const { currentData } = useFetchCurrentData("3"); // change depending on which id
+     const { currentData } = useFetchCurrentData(); // change depending on which id
 
      const [flattenDataArr, setFlattenDataArr] = useState<FullDataI[]>([])
 
@@ -43,21 +40,24 @@ function App() {
                               Bar Graph
                          </div>
                     </div>
-                    <div className="div4 border rounded p-4">
-                    <MyCard props={{    voltage: 2,
-                                        current: 3,
-                                        espClientId: 'string'}}/>
-                         {/* {
-                              allCurrentById !== null && allCurrentById.length > 0 ?
-                              // <div>asfalsas</div>
-                              // allCurrentById.map(item => (
-                                   <MyCard props={{    voltage: 2,
-                                        current: 3,
-                                        espClientId: 'string'}}/>
-                              // ))
-                               : 
-                              <div className="text-center mt-5">Loading ....</div>
-                         } */}
+                    <div className="div4 border rounded p-4 overflow-x-auto overflow-y-scroll">
+                         <div className="px-1 p-1 py-2 text-black fs-5">
+                              Current clients data
+                         </div>
+                         <div className="d-flex flex-wrap">
+                         {
+                        (currentData && currentData.length > 0) ? 
+                         currentData.map((item: CardsI) => (
+                              <MyCard props={{
+                                   voltage: item.voltage,
+                                   current: item.current,
+                                   espClientId: item.espClientId
+                              }} />
+                         ))
+                         : 
+                         <div className="text-center mt-5">Loading ....</div>
+                    }
+                         </div>
                     </div>
                </div>
           </>
--- src/hooks/useFetchCurrentData.tsx
+++ src/hooks/useFetchCurrentData.tsx
@@ -2,26 +2,23 @@ import { useCallback, useEffect, useState } from "react";
 import ApiService from "./useApi";
 import { CardsI } from "../components/Card";
 
-export interface CurrentDataI extends CardsI {
-     _id: string;
-}
 
-function useFetchCurrentData(id: string) {
+function useFetchCurrentData() {
      const { useApi } = ApiService();
-     const [currentData, setCurrentData] = useState<CurrentDataI | null>(null);
+     const [currentData, setCurrentData] = useState<CardsI[] | null>(null);
 
-     const fetchData = useCallback(async (id: string) => {
-          const api = useApi(`EE/current/${id}`);
+     const fetchData = useCallback(async () => {
+          const api = useApi(`EE/current/`);
           const resp = await api.get();
 
           if (resp) {
-            setCurrentData(resp.currentData);
+            setCurrentData(resp.current);
             return;
           }
      }, [currentData]);
 
      useEffect(() => {
-          fetchData(id);
+          fetchData();
      }, []);
 
      return { currentData };
