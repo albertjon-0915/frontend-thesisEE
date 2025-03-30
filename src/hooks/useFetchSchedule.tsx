d5c980ee77ad2262e2f1310040e944e8d579992f
albert.jon.inciong.0915 <albert.jon.inciong.0915@gmail.com>
2025-03-31 00:01:08 +0800

    add dialog confirm for sched

--- src/hooks/useFetchSchedule.tsx
+++ src/hooks/useFetchSchedule.tsx
@@ -1,21 +1,41 @@
 import ApiService from "./useApi";
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 function useFetchSchedule() {
-     const { useApi } = ApiService();
+    const { useApi } = ApiService();
+    const MySwal = withReactContent(Swal);
 
-     const setSchedule = async (minutes: number) => {
+    const setSchedule = async (minutes: number) => {
         const payload = { "minutes": minutes }
         const api = useApi(`EE/schedule/update`); 
         const resp = await api.patch(payload);
 
-        if (resp) return resp
+        if (Number(resp.minutes) > 0) {
+            // return resp
+            return MySwal.fire({
+                title: 'Schedule has been updated',
+                text: `${resp.message}`,
+                icon: 'success',
+                confirmButtonText: 'close',
+            });
+        }
+   
      }
 
-     const resetSchedule = async () => {
+    const resetSchedule = async () => {
         const api = useApi(`EE/schedule/reset`); 
         const resp = await api.post();
 
-        if (resp) return resp
+        if (resp.message === "Schedule has been reset") {
+            // return resp
+            return MySwal.fire({
+                title: "Schedule has been reset",
+                text: `${resp.message}`,
+                icon: 'success',
+                confirmButtonText: 'close',
+            });
+        }
      }
      
      return { setSchedule, resetSchedule };
